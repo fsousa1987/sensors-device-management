608704f7a72e46980372f436f42ccf48660085da
Francisco Sousa <franciscoeds1987@gmail.com>
2025-05-08 14:01:55 -0300

    Simplificando a implementação com Factory Pattern

--- src/main/java/com/francisco/sensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/francisco/sensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -1,28 +1,18 @@
 package com.francisco.sensors.device.management.api.client.impl;
 
+import com.francisco.sensors.device.management.api.client.RestClientFactory;
 import com.francisco.sensors.device.management.api.client.SensorMonitoringClient;
-import com.francisco.sensors.device.management.api.client.SensorMonitoringClientBadGatewayException;
 import io.hypersistence.tsid.TSID;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-
-import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 
 	private final RestClient restClient;
 
-	public SensorMonitoringClientImpl(RestClient.Builder builder) {
-		this.restClient = builder.baseUrl("http://localhost:8082")
-				.requestFactory(generateClientHttpRequestFactory())
-				.defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
-					throw new SensorMonitoringClientBadGatewayException();
-				})
-				.build();
+	public SensorMonitoringClientImpl(RestClientFactory factory) {
+		this.restClient = factory.temperatureMonitoringRestClient();
 	}
 
 	@Override
@@ -39,15 +29,6 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 				.uri("/api/sensors/{sensorId}/monitoring/enable", sensorId)
 				.retrieve()
 				.toBodilessEntity();
-	}
-
-	private ClientHttpRequestFactory generateClientHttpRequestFactory() {
-		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-
-		factory.setReadTimeout(Duration.ofSeconds(5));
-		factory.setConnectTimeout(Duration.ofSeconds(3));
-
-		return factory;
 	}
 
 }
